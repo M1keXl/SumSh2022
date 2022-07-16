8a8e3e2b697eaa516d7bf951ad6b91f3a0b7b528
M1keXl <mihayloh.com@gmail.com>
2022-07-16 20:18:47 +0300

    fix: changed 123.sh file

--- 123.sh
+++ 123.sh
@@ -2,6 +2,6 @@
 mkdir -p khliboyko/1
 mkdir -p khliboyko/2
 mkdir -p khliboyko/3
- echo " SummerS_2022 16.07.2022 mihayloh.com@gmail.com" >khliboyko/3/Mkhliboyko.txt
- echo " SummerS_2022 16.07.2022 mihayloh.com@gmail.com" >khliboyko/2/Mkhliboyko2.txt
+ echo " SummerSk_2022 16.07.2022 mihayloh.com@gmail.com" >khliboyko/3/Mkhliboyko.txt
+ echo " SummerSk_2022 16.07.2022 mihayloh.com@gmail.com" >khliboyko/2/Mkhliboyko2.txt
 
